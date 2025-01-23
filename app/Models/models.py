ffb655b220c460067fe4c762152c88dc16d03806
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2025-01-23 00:00:32 -0300

    Update models.py

--- app/Models/models.py
+++ app/Models/models.py
@@ -21,8 +21,6 @@ class Aluno(SQLModel, table=True):
     contato_email: str
     cursos: List["Inscricao"] = Relationship(back_populates="aluno")
     certificados: List["Certificado"] = Relationship(back_populates="aluno")
-    saldo: float = Field(default=0.0)  # Campo saldo com valor inicial de 0.0
-    # data_nascimento: Optional[datetime] = Field(default=None, nullable=True)  # Permite NULL
 
 class Aula(SQLModel, table=True):
     id_aula: Optional[int] = Field(default=None, primary_key=True)
